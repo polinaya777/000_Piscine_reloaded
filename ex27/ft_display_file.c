45e2dcf35c5c73fa30955256ce708f743ed3a7f5
Polina Yarova <pyarova@fu-r10-p2.s19.be>
2024-10-08 11:49:36 +0200

    ex00-ex27 v.1

---
+++ ex27/ft_display_file.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pyarova <pyarova@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 11:12:30 by pyarova           #+#    #+#             */
+/*   Updated: 2024/10/08 11:36:57 by pyarova          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void	ft_display_file(char *file_name)
+{
+	int		fd;
+	int		ret;
+	char	buff[4096];
+
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+	{
+		write(1, "Cannot read file.\n", 18);
+		return ;
+	}
+	while ((ret = read(fd, buff, 4096)) > 0)
+	{
+		write(1, buff, ret);
+	}
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		write(1, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(1, "Too many arguments.\n", 20);
+	else
+		ft_display_file(argv[1]);
+	return (0);
+}
